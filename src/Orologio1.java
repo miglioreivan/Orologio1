ef12a3ace48c09458b77f7500adeb303c6a43ade
Ivan Migliore <migliore.ivan.27@gmail.com>
2020-05-09 15:31:21 +0200

    y

--- src/Orologio1.java
+++ src/Orologio1.java
@@ -1,27 +1,32 @@
 import javax.swing.*;
 import java.util.Calendar;
+import java.util.Date;
 
 public class Orologio1 {
     private JPanel orologio;
     private JLabel anno;
-    private JLabel giorno;
     private JLabel ora;
     private JLabel min;
     private JLabel sec;
-    private String y, d, h, m, s;
-    Calendar now = Calendar.getInstance();
+    private String y;
+    private String d;
+    private String mh;
+    private String h;
+    private String m;
+    private String s;
+
+    Date dateOne = new Date();
 
     public Orologio1() {
 
-        y= String.valueOf(Calendar.YEAR);
-        m= String.valueOf(Calendar.MONTH);
-        d= String.valueOf(Calendar.DAY_OF_MONTH);
-        h=String.valueOf(Calendar.HOUR);
-        m=String.valueOf(Calendar.MINUTE);
-        s=String.valueOf(Calendar.SECOND);
+        y=String.valueOf(Calendar.getInstance().get(Calendar.YEAR));
+        mh=String.valueOf(Calendar.getInstance().get(Calendar.MONTH));
+        d=String.valueOf(Calendar.getInstance().get(Calendar.DAY_OF_MONTH));
+        h=String.valueOf(Calendar.getInstance().get(Calendar.HOUR));
+        m=String.valueOf(Calendar.getInstance().get(Calendar.MINUTE));
+        s=String.valueOf(Calendar.getInstance().get(Calendar.SECOND));
 
         anno.setText(y);
-        giorno.setText(d);
         ora.setText(h);
         min.setText(m);
         sec.setText(s);
@@ -35,6 +40,7 @@ public class Orologio1 {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
+
 
     }
 
